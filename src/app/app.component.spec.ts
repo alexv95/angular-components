4f7f44ac4d3ece325c90bfc8ebe65c5eca6eef17
alex martinez <alexmb1995@gmail.com>
2021-04-21 15:11:58 -0400

    :sparkles: add components to app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MenuComponent } from "./menu/menu.component";
+import { BodyComponent } from "./body/body.component";
+import { FooterComponent } from "./footer/footer.component";
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, MenuComponent, BodyComponent,FooterComponent
       ],
     }).compileComponents();
   });

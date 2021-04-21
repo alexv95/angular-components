0d590a1b373337e7171cfa39207160d1b8a80440
alex martinez <alexmb1995@gmail.com>
2021-04-21 15:06:50 -0400

    :sparkles: add 3 components to main project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
